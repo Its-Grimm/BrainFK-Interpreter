b1e6995e8ef71530a41d07289df48aa541756b2f
t <witiwtyler@gmail.com>
2025-01-27 13:14:17 -0800

    Added Sparc and X86 inside assembly. Started working on the C version

---
+++ C-Interpreter/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MEMORY_ARRAY_SIZE 30000
+
+
+const int memory_array_size = MEMORY_ARRAY_SIZE;
+// Prototypes
+
+
+int main() {
+   printf("Thing");
+
+
+   return 0;
+}
